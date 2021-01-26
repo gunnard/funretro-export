a7f907ead3a602adee38bb84ae48695429093c53
Gunnard Engebreth <gunnard.engebreth@powercode.com>
2021-01-26 14:17:33 -0600

    added json ability and logo

--- index.js
+++ index.js
@@ -2,11 +2,47 @@ const fs = require('fs');
 const path = require('path');
 const { chromium } = require('playwright');
 
-const [url, file] = process.argv.slice(2);
+var params = require('./data.json');
+var [url, file, fileType] = process.argv.slice(2);
+let date_ob = new Date();
+let date = ("0" + date_ob.getDate()).slice(-2);
+let month = ("0" + (date_ob.getMonth() + 1)).slice(-2);
+let year = date_ob.getFullYear();
+let hours = date_ob.getHours();
+let minutes = date_ob.getMinutes();
+let seconds = date_ob.getSeconds();
+var logo = params.logo;
+console.clear();
+console.log(`%c
+___________                          
+\\_   _____/_ __  ____                
+ |    __)|  |  \\/    \\               
+ |     \\ |  |  /   |  \\              
+ \\___  / |____/|___|  /              
+     \\/             \\/               
+__________        __                 
+\\______   \\ _____/  |________  ____  
+ |       _// __ \\   __\\_  __ \\/  _ \\ 
+ |    |   \\  ___/|  |  |  | \\(  <_> )
+ |____|_  /\\___  >__|  |__|   \\____/ 
+        \\/     \\/                    `, "font-family:monospace");
+
 
 if (!url) {
-  throw 'Please provide a URL as the first argument.';
+	url = params.url;
+	console.log('No URL specified, using: ' + url);
 }
+
+if (!file) {
+	file = params.file + "-" + year + "-" + month + "-" + date + "-" + hours + minutes + seconds;
+	console.log('No file name specified, using: '+ file);
+}
+
+if (!fileType) {
+	fileType = params.fileType;
+	console.log('No file type specified, using: '+ fileType);
+}
+process.exit();
 
 async function run() {
   const browser = await chromium.launch();
